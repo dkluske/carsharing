504e432c76fe922e8d92463158811d2c34e40859
Dome <Dome@LAPTOP-3CQ5VI03>
2019-01-07 20:46:52 +0100

    klassen und jsp

---
+++ src/java/CarSharing/logic/RentManager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CarSharing.logic;
+
+/**
+ *
+ * @author Dome
+ */
+public class RentManager {
+    //Vermietung
+    
+    //Freigabe
+    
+    //Miet-Log
+    
+    //Verfügbare Autos
+    
+    //Gemietete Autos
+}
