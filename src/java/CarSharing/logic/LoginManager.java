b4e070d78cf375fa7f08cd1a14f7a59c47159d77
OnTheRockz <OnTheRockz@DESKTOP-666CAQA>
2019-01-09 22:00:50 +0100

    Zwischenstand

--- src/java/CarSharing/logic/LoginManager.java
+++ src/java/CarSharing/logic/LoginManager.java
@@ -5,14 +5,62 @@
  */
 package CarSharing.logic;
 
+import CarSharing.datenbank.Datenbank;
+import CarSharing.model.Account;
+import CarSharing.model.Kunde;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
- * @author Dome
+ * @author Matti Düring
  */
 public class LoginManager {
-    //Login
     
-    //Übersicht gemietete Autos
+    public static Kunde getKunde(String accName){
+        Statement stm = Datenbank.getStatement();
+        String sql = "SELECT name, vorname FROM kunde WHERE kunde.account = '" + accName + "'";
+        
+        Kunde kunde = new Kunde();
+        int accId = getAccountId(accName);
+        
+        try {
+            ResultSet rs = stm.executeQuery(sql);
+            
+            while(rs.next()){
+                kunde.setName(rs.getString("name"));
+                kunde.setVorname(rs.getString("vorname"));
+                kunde.setAccount( new Account(accName, accId) );
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return kunde;
+    }
     
-    
+    private static int getAccountId(String accName){
+        Statement stm = Datenbank.getStatement();
+        String sql = "SELECT id FROM account WHERE account.name = '" + accName + "'";
+        
+        int id = 0;
+        
+        try {
+            ResultSet rs = stm.executeQuery(sql);
+            
+            while(rs.next()){
+                id = rs.getInt("id");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return id;
+    }
 }
